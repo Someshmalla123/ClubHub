8fbae923bd551558b67da44b731ccba4cfe1dddd
Someshmalla123 <venommalla@gmail.com>
2026-06-26 22:25:10 +0545

    VH-11 Implement addmember DAO and service

--- src/dao/MemberDAO.java
+++ src/dao/MemberDAO.java
@@ -1,14 +1,41 @@
 package dao;
 
+import database.DBConnection;
 import model.Member;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class MemberDAO {
 
     public boolean addMember(Member member) {
 
-        // Database code will go here
+        String sql = "INSERT INTO members(full_name, email, phone, position, username, password) VALUES (?, ?, ?, ?, ?, ?)";
 
-        return false;
+        try {
+            Connection conn = DBConnection.getConnection();
+
+            PreparedStatement pst = conn.prepareStatement(sql);
+
+            pst.setString(1, member.getFullName());
+            pst.setString(2, member.getEmail());
+            pst.setString(3, member.getPhone());
+            pst.setString(4, member.getPosition());
+            pst.setString(5, member.getUsername());
+            pst.setString(6, member.getPassword());
+
+            int rowsInserted = pst.executeUpdate();
+
+            conn.close();
+
+            return rowsInserted > 0;
+
+        } catch (SQLException e) {
+
+            System.out.println("Add Member Error: " + e.getMessage());
+
+            return false;
+        }
     }
-
 }
